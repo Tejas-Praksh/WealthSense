45e374096b90ea611a90515ff634c4f489b30a25
Tejas-Praksh <tp5063144@gmail.com>
2026-05-16 12:46:05 +0530

    fix: enable MongoDB repositories and add MONGO_URI to resolve AuditRepository dependency

--- user-service/src/main/java/com/wealthsense/user/UserServiceApplication.java
+++ user-service/src/main/java/com/wealthsense/user/UserServiceApplication.java
@@ -2,8 +2,12 @@ package com.wealthsense.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.wealthsense")
+@EnableJpaRepositories(basePackages = "com.wealthsense")
+@EnableMongoRepositories(basePackages = "com.wealthsense")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
